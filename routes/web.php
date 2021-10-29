5dee1d17e1f3688d997708c2060344d6d45fd2f4
unknown <hilmythoriq09@gmail.com>
2021-10-29 10:57:08 +0700

    middleware edit

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,13 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/hotel', [App\Http\Controllers\HotelController::class, 'index'])->name('hotel.index');
 
-Route::get('/hotel/create', [App\Http\Controllers\HotelController::class, 'create'])->name('hotel.create');
-Route::post('/hotel/store', [App\Http\Controllers\HotelController::class, 'store'])->name('hotel.store');
-Route::get('/hotel/{id}/edit', [App\Http\Controllers\HotelController::class, 'edit'])->name('hotel.edit');
-Route::put('/hotel/{id}/update', [App\Http\Controllers\HotelController::class, 'update'])->name('hotel.update');
-Route::delete('/hotel/{id}/delete', [App\Http\Controllers\HotelController::class, 'destroy'])->name('hotel.destroy');
+Route::group(['middleware'=>'auth','admin'],function(){
+    Route::get('/hotel', [App\Http\Controllers\HotelController::class, 'index'])->name('hotel.index');
+    Route::get('/hotel/create', [App\Http\Controllers\HotelController::class, 'create'])->name('hotel.create');
+    Route::post('/hotel/store', [App\Http\Controllers\HotelController::class, 'store'])->name('hotel.store');
+    Route::get('/hotel/{id}/edit', [App\Http\Controllers\HotelController::class, 'edit'])->name('hotel.edit');
+    Route::put('/hotel/{id}/update', [App\Http\Controllers\HotelController::class, 'update'])->name('hotel.update');
+    Route::delete('/hotel/{id}/delete', [App\Http\Controllers\HotelController::class, 'destroy'])->name('hotel.destroy');
+});
+
